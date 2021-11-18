a93c221dc7c96a68b47081f847e3b3f47962bcb5
RobAnKo <robinkoch92@gmail.com>
2021-11-18 10:41:38 +0100

    unfinished changes to dock_widgets

--- src/napari_image_stacker/_dock_widget.py
+++ src/napari_image_stacker/_dock_widget.py
@@ -7,11 +7,15 @@ see: https://napari.org/docs/dev/plugins/hook_specifications.html
 Replace code below according to your needs.
 """
 from napari_plugin_engine import napari_hook_implementation
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
+from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton, QCheckBox
 from magicgui import magic_factory
 
+#to test
+import datetime
+import pathlib
 
-class ExampleQWidget(QWidget):
+
+class QImageStackerWidget(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
@@ -19,23 +23,57 @@ class ExampleQWidget(QWidget):
     def __init__(self, napari_viewer):
         super().__init__()
         self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+        n_images = len(self.viewer.layers)
+        
+        visible_chkbx = QCheckBox("return visible?")
+        visible_chkbx.setChecked(False)
+        img2stack_btn = QPushButton("All open images to stack")
+        img2stack_btn.clicked.connect(self._on_click_i2s_button)
+        stack2img_btn = QPushButton("All open stacks to images")
+        stack2img_btn.clicked.connect(self._on_click_s2i_button)
 
         self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+        self.layout().addWidget(img2stack_btn)
+        self.layout().addWidget(stack2img_btn)
+        self.layout().addWidget(visible_chkbx)
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+    def _on_click_i2s_button(self):
+        layers = self.viewer.layers
+        valid = [layer for layer in layers if (layer.data.ndim == 2 and not layer.rgb)]
+        print(f"Converting all {len(valid)} open images to a stack")
+        #viewer.
+        
+    def _on_click_s2i_button(self):
+        layers = self.viewer.layers
+        valid = [layer for layer in layers if (layer.data.ndim == 2 and not layer.rgb)]
+        print(f"Converting all {len(valid)} open images to a stack")
+
+
+@magic_factory(
+    call_button="Calculate",
+    slider_float={"widget_type": "FloatSlider", 'max': 10},
+    dropdown={"choices": ['first', 'second', 'third']},
+)
+
+def widget_demo(
+    maybe: bool,
+    some_int: int,
+    spin_float=3.14159,
+    slider_float=4.5,
+    string="Text goes here",
+    dropdown='first',
+    date=datetime.datetime.now(),
+    filename=pathlib.Path('/some/path.ext')
+):
+    widget_demo.show()
 
 
 @magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
+def MImageStackerWidget(img_layer: "napari.layers.Image"):
     print(f"you have selected {img_layer}")
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     # you can return either a single widget, or a sequence of widgets
-    return [ExampleQWidget, example_magic_widget]
+    return [QImageStackerWidget, widget_demo, MImageStackerWidget]
